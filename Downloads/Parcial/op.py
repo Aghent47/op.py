4d6afbbb66d4d99f6daa73866737f0ed37605e45
Neider Hernandez <44579954+Aghent47@users.noreply.github.com>
2020-03-02 17:13:00 -0500

    agregando datos por consola

--- Downloads/Parcial/op.py
+++ Downloads/Parcial/op.py
@@ -1,3 +1,16 @@
 from argparse import ArgumentParser
 from sys import argv
 import os
+
+parser = ArgumentParser(description='programa de conjunto')
+parser.add_argument('-v','--variable', help="variables",nargs=2)
+parser.add_argument('-p','--union', help="union", type=int, default=2)
+args = parser.parse_args()
+if args.variable and args.union:
+      print(args.variable)
+      print(args.union)
+
+
+
+ 
+ 
